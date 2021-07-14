a328f63885038973a171384337184b699a864611
Sawyer Cherry <sawyeracherry@gmail.com>
2021-07-13 20:13:52 -0500

    push

--- WillMe iOS/Controller/SetupProfileViewController.swift
+++ WillMe iOS/Controller/SetupProfileViewController.swift
@@ -14,11 +14,21 @@ class SetupProfileViewController: UIViewController {
     var managedContext: NSManagedObjectContext!
     
     
+    let container: UIStackView = {
+        let container = UIStackView()
+        container.axis = .vertical
+        container.spacing = 0
+        container.distribution = .fillEqually
+        container.translatesAutoresizingMaskIntoConstraints = false
+        return container
+    }()
     
     let welcomeMessage: UILabel = {
         let lbl = UILabel()
         lbl.translatesAutoresizingMaskIntoConstraints = false
         lbl.textAlignment = .center
+        lbl.text = "Welcome to willMe."
+        lbl.font = UIFont(name: "Helvetica", size: 20.0)
         return lbl
     }()
     
@@ -26,6 +36,7 @@ class SetupProfileViewController: UIViewController {
         let first = UITextField()
         first.translatesAutoresizingMaskIntoConstraints = false
         first.textAlignment = .center
+        first.placeholder = "First Name"
         return first
     }()
     
@@ -33,6 +44,7 @@ class SetupProfileViewController: UIViewController {
         let last = UITextField()
         last.translatesAutoresizingMaskIntoConstraints = false
         last.textAlignment = .center
+        last.placeholder = "Last Name"
         return last
     }()
     
@@ -41,13 +53,14 @@ class SetupProfileViewController: UIViewController {
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.textAlignment = .center
         textField.isSecureTextEntry = true
+        textField.placeholder = "Social Security Number"
         return textField
     }()
     
     lazy var dOBField: UIDatePicker = {
         let datePicker = UIDatePicker()
         datePicker.translatesAutoresizingMaskIntoConstraints = false
-        datePicker.frame = CGRect(x: 10, y: 50, width: self.view.frame.width, height: 200)
+//        datePicker.frame = CGRect(x: 10, y: 50, width: self.view.frame.width, height: 200)
         datePicker.datePickerMode = .date
         let calendar = Calendar(identifier: .gregorian)
         var comps = DateComponents()
@@ -59,9 +72,6 @@ class SetupProfileViewController: UIViewController {
     }()
     
     
-    
-    
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -70,6 +80,7 @@ class SetupProfileViewController: UIViewController {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
         managedContext = appDelegate.persistentContainer.viewContext
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(saveButtonPressed))
+        setupUI()
         
     }
     
@@ -77,15 +88,28 @@ class SetupProfileViewController: UIViewController {
         self.navigationController?.topViewController?.dismiss(animated: true, completion: nil)
         
     }
+
     
-    func setWelcomeMessage(){
-        self.view.addSubview(welcomeMessage)
-        welcomeMessage.text = "Welcome to willMe, we are happy to have you!"
-        welcomeMessage.font = UIFont(name: "Helvetica", size: 20.0)
-//        NSLayoutConstraint.activate ([
-//            welcomeMessage.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-//            welcomeMessage.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
-//        ])
+    func setupUI(){
+        self.view.addSubview(container)
+    
+        NSLayoutConstraint.activate ([
+//            container.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+//            container.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+//            container.heightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.8),
+//            container.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8)
+            container.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            container.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
+            container.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant: 5),
+            container.rightAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.rightAnchor, constant: -5),
+        ])
+        container.addArrangedSubview(welcomeMessage)
+        container.addArrangedSubview(firstNameField)
+        container.addArrangedSubview(lastNameField)
+        container.addArrangedSubview(ssnField)
+        container.addArrangedSubview(dOBField)
     }
+    
+    
 
 }
